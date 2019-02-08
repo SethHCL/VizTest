df364554ff4988ec2b49d38f8fb819312383333e
SethKony <seth.weissman@kony.com>
2019-02-08 09:08:23 -0700

    new form

---
+++ controllers/mobile/Form1Controller/Form1ControllerActions.js
@@ -0,0 +1,10 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onClick defined for Button0efa11b6f6fb146 **/
+    AS_Button_j44b60171e4f4569b589b169e6fa0325: function AS_Button_j44b60171e4f4569b589b169e6fa0325(eventobject) {
+        var self = this;
+        alert("Hello World!");
+    }
+});
